f032fc7fc3626c634bdccf45a41f87eec2266bfc
PJWP <paulwp06@outlook.com>
2026-01-19 19:40:45 +0000

    Updated to include V and X characters

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,2 +1,14 @@
+ROMAN_PAIRS = [("X",10),("V",5),("I",1)]
+
 def to_roman(num):
-    return "I"
+    result = []
+    remaining = num
+    for symbol, value in ROMAN_PAIRS:
+        count = remaining // value
+        remaining = remaining % value
+        if count > 0:
+            result.append(symbol*count)
+        if remaining == 0:
+            break
+    return "".join(result)
+
--- test_roman_numeral.py
+++ test_roman_numeral.py
@@ -5,3 +5,17 @@ def test_smoke_test():
 
 def test_1_is_I():
     assert to_roman(1) == "I"
+
+def test_3_is_III():
+    assert to_roman(3) == "III"
+
+def test_2_is_II():
+    assert to_roman(2) == "II"
+
+def test_5_is_V_and_6_is_VI():
+    assert to_roman(5) == "V"
+    assert to_roman(6) == "VI"
+
+def test_10_is_X_and_12_is_XII():
+    assert to_roman(10) == "X"
+    assert to_roman(12) == "XII"
